abca27e1d553a7653b82f01fe5df147c3cfde758
Dmitry Permyakov <ddmitiy@mail.ru>
2023-01-26 13:51:52 +0300

    Add requirements

--- core/settings.py
+++ core/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_elasticsearch_dsl',
     'blog',
+
     'rest_framework',
     'search',
+
 
 ]
 
